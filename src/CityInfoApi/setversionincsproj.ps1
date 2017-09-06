f496ec808c663badb783d1dea1dfd94036bf76e4
Sergii Kryshtop <ksa@3si2.com>
2017-09-06 09:46:33 +0300

    Powershell script to replace assembly and file version in csproj file at build time

---
+++ src/CityInfoApi/setversionincsproj.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$true, Position=1)]
+	$version,
+    [Parameter(Mandatory=$false, Position=2)]
+	$filePath = ".\CityInfo.Api\CityInfo.Api.csproj"
+)
+
+
+
+[xml]$xml = Get-Content $filePath
+$propertyGroup = $xml.Project.PropertyGroup;
+
+if($propertyGroup)
+{
+	$propertyGroup["AssemblyVersion"]."#text" = "$version"
+	$propertyGroup["FileVersion"]."#text" = "$version"
+	
+}
+$xml.Save($filePath)
